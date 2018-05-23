bf2947eb6b157e60f29f256242c0ca50e85a58d7
Maciej Matczak <pan.ellox@gmail.com>
2018-05-23 17:09:20 +0200

    Add partial transaction api

--- app/api.py
+++ app/api.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, request
 from flask_restful import Api, Resource
 from marshmallow import Schema, fields, post_load
 
@@ -9,8 +9,8 @@ api = Api(bp)
 
 
 class CategorySchema(Schema):
-    id = fields.Int(dump_only=True)
-    name = fields.Str(required=True)
+    id = fields.Integer(dump_only=True)
+    name = fields.String(required=True)
     date = fields.Date(required=True)
     estimate = fields.Float(required=True)
 
@@ -21,6 +21,31 @@ class CategorySchema(Schema):
 
 category_schema = CategorySchema()
 categories_schema = CategorySchema(many=True)
+
+
+class TransactionSchema(Schema):
+    id = fields.Int(dump_only=True)
+    new = fields.Boolean()
+    account_date = fields.Date(required=True)
+    operation_date = fields.Date()
+    details = fields.String()
+    account_no = fields.String()
+    title = fields.String()
+    amount = fields.Float(required=True)
+    currency = fields.String()
+    ref_number = fields.String()
+    operation_type = fields.String()
+    note = fields.String()
+    category = fields.Nested(CategorySchema, dump_only=True)
+    category_id = fields.Integer()
+
+    @post_load
+    def make_model(self, data):
+        return Transaction(**data)
+
+
+transaction_schema = TransactionSchema()
+transactions_schema = TransactionSchema(many=True)
 
 
 class CategoryListResource(Resource):
@@ -62,22 +87,23 @@ class CategoryResource(Resource):
 
 
 class TransactionListResource(Resource):
-    def get(self, id):
-        transaction = Transaction.query.get_or_404(id)
-        return jsonify({
-            'transaction': transaction
-        })
+    def get(self):
+        transactions = Transaction.query.all()
+        result = transactions_schema.dump(transactions)
+
+        return result
 
     def post(self):
-        pass
+        json = request.get_json()
+        result = transaction_schema.load(json)
+
+        db.session.add(result.data)
+        db.session.commit()
 
 
 class TransactionResource(Resource):
     def get(self, id):
-        transaction = Transaction.query.get_or_404(id)
-        return jsonify({
-            'transaction': transaction
-        })
+        pass
 
     def put(self, id):
         pass
@@ -89,3 +115,4 @@ class TransactionResource(Resource):
 api.add_resource(CategoryResource, '/categories/<int:id>')
 api.add_resource(CategoryListResource, '/categories')
 api.add_resource(TransactionResource, '/transactions/<int:id>')
+api.add_resource(TransactionListResource, '/transactions')
