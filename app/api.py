e1f2cbed8b3e0fc4e7bc75e83f5fa1ccdb945ab6
Maciej Matczak <pan.ellox@gmail.com>
2018-06-07 19:00:02 +0200

    Add and define "beta" version of models to use in app

--- app/api.py
+++ app/api.py
@@ -2,25 +2,10 @@ from flask import Blueprint, request
 from flask_restful import Api, Resource
 from marshmallow import Schema, fields, post_load
 
-from .models import db, Category, Transaction
+from app.models import db, Category, ParentCategory, Budget, Transaction
 
 bp = Blueprint('api', __name__)
 api = Api(bp)
-
-
-class CategorySchema(Schema):
-    id = fields.Integer(dump_only=True)
-    name = fields.String(required=True)
-    date = fields.Date(required=True)
-    estimate = fields.Float(required=True)
-
-    @post_load
-    def make_model(self, data):
-        return Category(**data)
-
-
-category_schema = CategorySchema()
-categories_schema = CategorySchema(many=True)
 
 
 class TransactionSchema(Schema):
@@ -36,7 +21,7 @@ class TransactionSchema(Schema):
     ref_number = fields.String()
     operation_type = fields.String()
     note = fields.String()
-    category = fields.Nested(CategorySchema, dump_only=True)
+    category = fields.Nested('CategorySchema', dump_only=True)
     category_id = fields.Integer()
 
     @post_load
@@ -48,10 +33,52 @@ transaction_schema = TransactionSchema()
 transactions_schema = TransactionSchema(many=True)
 
 
+class ParentCategorySchema(Schema):
+    id = fields.Integer(dump_only=True)
+    name = fields.String(required=True)
+
+    @post_load
+    def make_model(self, data):
+        return ParentCategory(**data)
+
+
+parent_category_schema = ParentCategorySchema()
+parent_categories_schema = ParentCategorySchema(many=True)
+
+
+class CategorySchema(Schema):
+    id = fields.Integer(dump_only=True)
+    name = fields.String(required=True)
+    parent = fields.Nested(ParentCategorySchema)
+
+    @post_load
+    def make_model(self, data):
+        return Category(**data)
+
+
+category_schema = CategorySchema()
+categories_schema = CategorySchema(many=True)
+
+
+class BudgetSchema(Schema):
+    id = fields.Integer(dump_only=True)
+    date = fields.Date(required=True)
+    estimate = fields.Float(required=True)
+    category = fields.Nested(CategorySchema, required=True)
+
+    @post_load
+    def make_model(self, data):
+        return Budget(**data)
+
+
+budget_schema = BudgetSchema()
+budgets_schema = BudgetSchema(many=True)
+
+
 class CategoryListResource(Resource):
     def get(self):
-        categories = Category.query.all()
-        result = categories_schema.dump(categories)
+        item = Category.query.all()
+        result = categories_schema.dump(item)
 
         return result
 
@@ -72,10 +99,10 @@ class CategoryResource(Resource):
 
     def put(self, id):
         json = request.get_json()
-        new_data, _ = category_schema.load(json)
+        updated_item, _ = category_schema.load(json)
 
         category = Category.query.get_or_404(id)
-        for attr, value in new_data.items():
+        for attr, value in json.items():
             setattr(category, attr, value)
 
         db.session.commit()
@@ -86,10 +113,48 @@ class CategoryResource(Resource):
         db.session.commit()
 
 
+class ParentCategoryListResource(Resource):
+    def get(self):
+        item = ParentCategory.query.all()
+        result = parent_categories_schema.dump(item)
+
+        return result
+
+    def post(self):
+        json = request.get_json()
+        result = parent_category_schema.load(json)
+
+        db.session.add(result.data)
+        db.session.commit()
+
+
+class ParentCategoryResource(Resource):
+    def get(self, id):
+        item = ParentCategory.query.get_or_404(id)
+        result = parent_category_schema.dump(item)
+
+        return result
+
+    def put(self, id):
+        json = request.get_json()
+        new_data, _ = parent_category_schema.load(json)
+
+        item = ParentCategory.query.get_or_404(id)
+        for attr, value in json.items():
+            setattr(item, attr, value)
+
+        db.session.commit()
+
+    def delete(self, id):
+        item = ParentCategory.query.get_or_404(id)
+        db.session.delete(item)
+        db.session.commit()
+
+
 class TransactionListResource(Resource):
     def get(self):
-        transactions = Transaction.query.all()
-        result = transactions_schema.dump(transactions)
+        item = Transaction.query.all()
+        result = transactions_schema.dump(item)
 
         return result
 
@@ -103,16 +168,70 @@ class TransactionListResource(Resource):
 
 class TransactionResource(Resource):
     def get(self, id):
-        pass
+        item = Transaction.query.get_or_404(id)
+        result = transaction_schema.dump(item)
+
+        return result
 
     def put(self, id):
-        pass
+        json = request.get_json()
+        new_data, _ = transaction_schema.load(json)
+
+        item = Transaction.query.get_or_404(id)
+        for attr, value in json.items():
+            setattr(item, attr, value)
+
+        db.session.commit()
 
     def delete(self, id):
-        pass
+        item = Transaction.query.get_or_404(id)
+        db.session.delete(item)
+        db.session.commit()
+
+
+class BudgetListResource(Resource):
+    def get(self):
+        item = Budget.query.all()
+        result = budgets_schema.dump(item)
+
+        return result
+
+    def post(self):
+        json = request.get_json()
+        result = budget_schema.load(json)
+
+        db.session.add(result.data)
+        db.session.commit()
+
+
+class BudgetResource(Resource):
+    def get(self, id):
+        item = Budget.query.get_or_404(id)
+        result = budget_schema.dump(item)
+
+        return result
+
+    def put(self, id):
+        json = request.get_json()
+        new_data, _ = budget_schema.load(json)
+
+        item = Budget.query.get_or_404(id)
+        for attr, value in json.items():
+            setattr(item, attr, value)
+
+        db.session.commit()
+
+    def delete(self, id):
+        item = Budget.query.get_or_404(id)
+        db.session.delete(item)
+        db.session.commit()
 
 
 api.add_resource(CategoryResource, '/categories/<int:id>')
 api.add_resource(CategoryListResource, '/categories')
+api.add_resource(ParentCategoryResource, '/parent-categories/<int:id>')
+api.add_resource(ParentCategoryListResource, '/parent-categories')
+api.add_resource(BudgetResource, '/budgets/<int:id>')
+api.add_resource(BudgetListResource, '/budgets')
 api.add_resource(TransactionResource, '/transactions/<int:id>')
 api.add_resource(TransactionListResource, '/transactions')
--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ import click
 from flask.cli import with_appcontext
 from flask_sqlalchemy import SQLAlchemy, event
 from sqlalchemy.engine import Engine
+from sqlalchemy.sql import func, and_, extract
 
 
 db = SQLAlchemy()
@@ -14,45 +15,10 @@ def set_sqlite_pragma(dbapi_connection, connection_record):
     cursor.close()
 
 
-class Category(db.Model):
-    __tablename__ = 'categories'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    estimate = db.Column(db.Float, nullable=False)
-
-
-# class SubCategory(db.Model):
-#     __tablename__ = 'subcategories'
-
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String, nullable=False)
-#     date = db.Column(db.Date, nullable=False)
-#     estimate = db.Column(db.Float, nullable=False)
-#     category_id = db.Column(
-#         db.Integer,
-#         db.ForeignKey('categories.id', onupdate='CASCADE', ondelete='SET NULL')
-#     )
-#     category = db.relationship('Category', backref='subcategories')
-
-
-class BudgetRule(db.Model):
-    __tablename__ = 'budget_rules'
-    id = db.Column(db.Integer, primary_key=True)
-    sentence = db.Column(db.String, nullable=False)
-    estimate = db.Column(db.Float, nullable=False)
-    category_id = db.Column(
-        db.Integer,
-        db.ForeignKey('categories.id', onupdate='CASCADE', ondelete='SET NULL')
-    )
-    category = db.relationship('Category')
-
-
 class Transaction(db.Model):
     __tablename__ = 'transactions'
 
     id = db.Column(db.Integer, primary_key=True)
-    new = db.Column(db.Boolean)
     account_date = db.Column(db.Date, nullable=False)
     operation_date = db.Column(db.Date)
     details = db.Column(db.String)
@@ -65,50 +31,170 @@ class Transaction(db.Model):
     note = db.Column(db.String)
     category_id = db.Column(
         db.Integer,
-        db.ForeignKey('categories.id', onupdate='CASCADE', ondelete='SET NULL')
+        db.ForeignKey('categories.id', onupdate='CASCADE', ondelete='SET NULL'),
+        default=1
     )
     category = db.relationship('Category')
 
+    def __repr__(self):
+        return f'<Transaction {self.id}; {self.account_date} {self.amount:.2f} @ {self.category.name}>'
 
-def mockup(db):
+
+class ParentCategory(db.Model):
+    __tablename__ = 'parent_categories'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False, unique=True)
+
+    def __repr__(self):
+        return f'<ParentCategory {self.id} "{self.name}">'
+
+
+class Category(db.Model):
+    __tablename__ = 'categories'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False, unique=True)
+    parent_id = db.Column(
+        db.Integer,
+        db.ForeignKey('parent_categories.id', onupdate='CASCADE', ondelete='SET NULL')
+    )
+    parent = db.relationship('ParentCategory')
+
+    def __repr__(self):
+        return f'<Category {self.id} "{self.name}">'
+
+
+class Budget(db.Model):
+    __tablename__ = 'budgets'
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date, nullable=False)
+    estimate = db.Column(db.Float, nullable=False)
+    category_id = db.Column(
+        db.Integer,
+        db.ForeignKey('categories.id', onupdate='CASCADE', ondelete='SET NULL'),
+        nullable=False
+    )
+    category = db.relationship('Category')
+
+    def __repr__(self):
+        return f'<Budget {self.id}; {self.date} {self.estimate} "{self.category}">'
+
+
+def mock():
     from datetime import date, timedelta
-    import random
 
-    for name in ['?', 'Car', 'Grocery']:
+    bills = ParentCategory(
+        name='Bills'
+    )
+
+    db.session.add(bills)
+
+    categories = {
+        'Car': None,
+        'Grocery': None,
+        'TV Multimedia Internet': bills,
+        'Flat rent': bills,
+    }
+    for name, parent in categories.items():
         category = Category(
             name=name,
-            estimate=-12.34
+            parent=parent
         )
 
         db.session.add(category)
-
-    db.session.commit()
 
     categories = Category.query.all()
 
     for i in range(30):
         transaction = Transaction(
             account_date=date(2018, 5, 1) + timedelta(days=i),
-            category=random.choice(categories),
-            amount=-12.34
+            category=categories[i % len(categories)],
+            amount=-10
         )
 
         db.session.add(transaction)
 
+    for category in categories:
+        budget = Budget(
+            category=category,
+            date=date(2018, 5, 1),
+            estimate=-100
+        )
+
+        db.session.add(budget)
+
+    db.session.commit()
+
+    year = 2018
+    month = 5
+
+    print('Sub category summaries:')
+    for parent in ParentCategory.query.all():
+        print(parent.name + ':')
+        q = db.session.query(
+            Category.name, func.sum(Transaction.amount), Budget.estimate
+        ).join(ParentCategory).join(Budget).join(Transaction).group_by(Category).filter(
+            and_(
+                ParentCategory.id == parent.id,
+                extract('year', Transaction.account_date) == year,
+                extract('month', Transaction.account_date) == month
+            )
+        )
+        print(q.all())
+
+    print('Main summary:')
+
+    q = db.session.query(
+        ParentCategory.name, func.sum(Transaction.amount).label('transaction_sum'), Budget.estimate
+    ).join(Category).join(Budget).join(Transaction).group_by(ParentCategory).filter(
+        and_(
+            ParentCategory.id.isnot(None),
+            extract('year', Transaction.account_date) == year,
+            extract('month', Transaction.account_date) == month
+        )
+    ).subquery()
+
+    q2 = db.session.query(
+        q.c.name, func.sum(q.c.estimate), func.sum(q.c.transaction_sum)
+    ).group_by(q.c.name)
+    parent_category_summary = q2.all()
+
+    q = db.session.query(
+        Category.name, func.sum(Transaction.amount), Budget.estimate
+    ).join(Transaction).join(Budget).group_by(Category.id).filter(
+        and_(
+            Category.parent_id.is_(None),
+            extract('year', Transaction.account_date) == year,
+            extract('month', Transaction.account_date) == month
+        )
+    )
+    rest_category_summary = q.all()
+
+    print(parent_category_summary + rest_category_summary)
+
+
+def init_db():
+    db.drop_all()
+    db.create_all()
+
+    category = Category(id=1, name='?')
+    db.session.add(category)
     db.session.commit()
 
 
 @click.command('init-db')
 @with_appcontext
 def init_db_command():
-    """Clear existing data and create new tables."""
-    db.drop_all()
-    db.create_all()
+    init_db()
+
     click.echo('Initialized the database.')
 
 
 @click.command('mock-db')
 @with_appcontext
 def mock_db_command():
-    mockup(db)
+    init_db()
+    mock()
     click.echo('Mocked up the database.')
